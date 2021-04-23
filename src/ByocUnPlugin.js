5b1db83a4a2c09befb5d4d2fa8834ece8c155b63
nmora53 <nicman16216@gmail.com>
2021-04-23 17:08:08 -0300

    First commit

---
+++ src/ByocUnPlugin.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { VERSION } from '@twilio/flex-ui';
+import { FlexPlugin } from 'flex-plugin';
+
+import CustomTaskListContainer from './components/CustomTaskList/CustomTaskList.Container';
+import reducers, { namespace } from './states';
+
+const PLUGIN_NAME = 'ByocUnPlugin';
+
+export default class ByocUnPlugin extends FlexPlugin {
+  constructor() {
+    super(PLUGIN_NAME);
+  }
+
+  /**
+   * This code is run when your plugin is being started
+   * Use this to modify any UI components or attach to the actions framework
+   *
+   * @param flex { typeof import('@twilio/flex-ui') }
+   * @param manager { import('@twilio/flex-ui').Manager }
+   */
+  init(flex, manager) {
+    this.registerReducers(manager);
+
+    flex.Actions.addListener("beforeStartOutboundCall", (payload, abortFunction) => {
+      // check if calling Queue is SIP
+      if (payload.queueSid === "WQ9f0b9440750dbb413dcff53df5e76018"){
+       console.log("number to call:", "sip:"+payload.destination+"@201.234.139.138")
+       payload.destination = "sip:"+payload.destination+"@201.234.139.138"
+       payload.callerId = "+548008000016"
+     } else{
+       console.log("Non SIP Call, only changing CLI")
+       payload.callerId = "+548008000016"
+     }
+      console.log("updated outbound call to:", payload)
+      
+   })
+   
+  }
+
+  //before payload.callerId = "+541150771270" 
+  /**
+   * Registers the plugin reducers
+   *
+   * @param manager { Flex.Manager }
+   */
+  registerReducers(manager) {
+    if (!manager.store.addReducer) {
+      // eslint: disable-next-line
+      console.error(`You need FlexUI > 1.9.0 to use built-in redux; you are currently on ${VERSION}`);
+      return;
+    }
+
+    manager.store.addReducer(namespace, reducers);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as FlexPlugin from 'flex-plugin';
+import ByocUnPlugin from './ByocUnPlugin';
+
+FlexPlugin.loadPlugin(ByocUnPlugin);
