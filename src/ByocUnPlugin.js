b5001dc3cc41824a4e0552302df1d4baca809b77
nmora53 <nicman16216@gmail.com>
2021-12-23 14:59:14 -0300

    Update ByocUnPlugin.js

--- src/ByocUnPlugin.js
+++ src/ByocUnPlugin.js
@@ -25,12 +25,12 @@ export default class ByocUnPlugin extends FlexPlugin {
     flex.Actions.addListener("beforeStartOutboundCall", (payload, abortFunction) => {
       // check if calling Queue is SIP
       if (payload.queueSid === "WQ9f0b9440750dbb413dcff53df5e76018"){
-       console.log("number to call:", "sip:"+payload.destination+"@201.234.139.138")
-       payload.destination = "sip:"+payload.destination+"@201.234.139.138"
-       payload.callerId = "+548008000016"
+       console.log("number to call:", "sip:"+payload.destination+"@customer_IP_SBC")
+       payload.destination = "sip:"+payload.destination+"@2customer_IP_SBC"
+       payload.callerId = "+54xxxxx"
      } else{
        console.log("Non SIP Call, only changing CLI")
-       payload.callerId = "+548008000016"
+       payload.callerId = "+548xxxxx"
      }
       console.log("updated outbound call to:", payload)
       
